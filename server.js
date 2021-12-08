798347018632d00545f29fc3303aa00ad532c15b
Rangga Baghas <rbaghas32@gmail.com>
2021-12-08 21:34:38 +0700

    make it better

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ routes(app);
 //daftarkan menu routes dari index
 app.use("/auth", require("./middleware"));
 
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
